c16a3e7d00dcf5e465272fd64d7fb89cf9c065ca
wlvlrtm <kimsk200012@gmail.com>
2021-07-10 23:04:30 +0900

    2908 & 5622 add

---
+++ Java Baekjoon/src/b2908/main_2908.java
@@ -0,0 +1,46 @@
+package b2908;
+
+import java.util.Scanner;
+
+public class main_2908 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        // 사용자 입력; 상수 2개
+        String num_A = scanner.next();
+        String num_B = scanner.next();
+
+        // 상수 슬라이싱
+        String[] list_A = num_A.split("");
+        String[] list_B = num_B.split("");
+
+        // 뒤집은 상수 2개 저장; string 타입
+        String reverse_A = "";
+        String reverse_B = "";
+
+
+        // 상수 A reverse
+        for (int i = list_A.length-1; i >= 0; i--) {
+            reverse_A += list_A[i];
+        }
+
+
+        // 상수 B reverse
+        for (int j = list_B.length-1; j >= 0; j--) {
+            reverse_B += list_B[j];
+        }
+
+
+        // 크기 비교 후 결과 출력
+        int result_A = Integer.parseInt(reverse_A);
+        int result_B = Integer.parseInt(reverse_B);
+
+        if (result_A > result_B) {
+            System.out.print(result_A);
+        }
+        else {
+            System.out.print(result_B);
+        }
+    }
+}
---
+++ Java Baekjoon/src/b5622/main_5622.java
@@ -0,0 +1,24 @@
+package b5622;
+
+///
+// 1
+// 2: ABC
+// 3: DEF
+// 4: GHI
+// 5: JKL
+// 6: MNO
+// 7: PQRS
+// 8: TUV
+// 9: WXYZ
+// 0
+///
+
+
+
+
+public class main_5622 {
+    public static void main(String[] args) {
+
+    }
+
+}
