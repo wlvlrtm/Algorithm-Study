e8eff9a108614b9b70e112507da29900fac8caf5
wlvlrtm <kimsk200012@gmail.com>
2021-08-07 19:31:54 +0900

    package update

---
+++ Java Baekjoon/src/b2942/main_2942.java
@@ -0,0 +1,4 @@
+package b2942;
+
+public class main_2942 {
+}
--- Java Baekjoon/src/b5622/main_5622.java
+++ Java Baekjoon/src/b5622/main_5622.java
@@ -1,24 +1,51 @@
 package b5622;
 
-///
-// 1
-// 2: ABC
-// 3: DEF
-// 4: GHI
-// 5: JKL
-// 6: MNO
-// 7: PQRS
-// 8: TUV
-// 9: WXYZ
-// 0
-///
-
-
-
+import java.util.Scanner;
 
 public class main_5622 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int result = 0;
+
+
+        // num || Char
+        String[][] numToChar = {
+                {"A", "B", "C", "-"},
+                {"D", "E", "F", "-"},
+                {"G", "H", "I", "-"},
+                {"J", "K", "L", "-"},
+                {"M", "N", "O", "-"},
+                {"P", "Q", "R", "S"},
+                {"T", "U", "V", "-"},
+                {"W", "X", "Y", "Z"}
+        };
+
+
+        // User Input
+        String callNum;
+        callNum = scanner.next();
+
+        // String Splint
+        String[] cnSplint;
+        cnSplint = callNum.split("");
+
+
+        Loop1 : // One by one from cnSplint's element
+        for (String num : cnSplint) {
+            Loop2 : // Checking about is it equals num with callNum; Loop in numToChar's row.
+            for (int i = 0; i < numToChar.length; i++) {
+                Loop3 : //  Checking about is it equals num with callNum; Loop in numToChar's column.
+                for (int j = 0; j < numToChar[0].length; j++) {
+                    if (numToChar[i][j].equals(num)) {  // if num and callNum is equal,
+                        result += (i + 3);      // cal. time
+
+                        break Loop2;            // break
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);   // Print Result
     }
-
 }
