471809750cecfb7ae9b19d75d370142591d62997
wlvlrtm <kimsk200012@gmail.com>
2021-08-07 22:49:16 +0900

    2021.08.07 Update

---
+++ Java Baekjoon/src/b1316/main_1316.java
@@ -0,0 +1,10 @@
+package b1316;
+
+public class main_1316 {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
--- Java Baekjoon/src/b2942/main_2942.java
+++ Java Baekjoon/src/b2942/main_2942.java
@@ -1,4 +1,65 @@
 package b2942;
 
+import java.util.Scanner;
+
 public class main_2942 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 0;
+
+        // User Input
+        String str = input.nextLine();
+
+
+        // find Croatia's alphabets
+        for (int i = 0; i < str.length(); i++) {
+            char temp = str.charAt(i);
+
+            switch(temp) {
+                case 'c':
+                    if (i+1 < str.length()) {
+                        if (str.charAt(i+1) == '=' || str.charAt(i+1) == '-') {
+                            i += 1;
+                        }
+                    }
+                    break;
+
+                case 'd':
+                    if (i+1 < str.length()) {
+                        if (str.charAt(i+1) == 'z') {
+                            if (i+2 < str.length()) {
+                                if (str.charAt(i+2) == '=') {
+                                    i += 2;
+                                }
+                            }
+                        }
+                        else if (str.charAt(i+1) == '-') {
+                            i += 1;
+                        }
+                    }
+                    break;
+
+                case 'n':
+                case 'l':
+                    if (i+1 < str.length()) {
+                        if (str.charAt(i+1) == 'j') {
+                            i += 1;
+                        }
+                    }
+                    break;
+                case 'z':
+                case 's':
+                    if (i+1 < str.length()) {
+                        if (str.charAt(i+1) == '=') {
+                            i += 1;
+                        }
+                    }
+                    break;
+            }
+            count++;
+        }
+
+        // Result
+        System.out.println(count);
+    }
 }
