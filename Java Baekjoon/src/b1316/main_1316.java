21cdd5af884a069d0b2e2e2c53bbc33c294dbde2
wlvlrtm <kimsk200012@gmail.com>
2021-08-10 20:31:03 +0900

    1316 Update

--- Java Baekjoon/src/b1316/main_1316.java
+++ Java Baekjoon/src/b1316/main_1316.java
@@ -1,10 +1,55 @@
 package b1316;
 
+import java.util.Scanner;
+
 public class main_1316 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // group words count
+        int result = 0;
+
+        // User Input; number of words
+        int count = input.nextInt();
+
+
+        // User Input; words
+        for (int i = 0; i < count; i++) {
+            String word = input.next();
+
+            if (check(word)) {
+                result += 1;
+            }
+        }
+
+
+        // Result print
+        System.out.println(result);
+    }
 
 
 
+    public static boolean check(String word) {
+        boolean[] alphabets = new boolean[26];  // array of alphabets (26: A to Z)
+        char preTemp = ' '; // front char
 
+        for (int i = 0; i < word.length(); i++) {
+            char temp = word.charAt(i); // current char
+
+            // if the same char doesn't repeat,
+            if (temp != preTemp) {
+                // if current char is new one,
+                if (!alphabets[(int)temp - 'a']) {
+                    alphabets[(int)temp - 'a'] = true;  // change the array's state.
+                    preTemp = temp; // change the front char
+                }
+                else {  // if current char is same of front char even though they didn't repeat; if that word is not group word,
+                    return false;   // false; It's not a group word
+                }
+            }
+            // else: if the same character is repeatedly, -> continue
+        }
+
+        return true;    // It's a group word
     }
 }
