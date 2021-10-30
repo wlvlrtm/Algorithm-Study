c5507f31124a949c824b9043187a5947085a744b
wlvlrtm <kimsk200012@gmail.com>
2021-10-30 20:54:31 +0900

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,29 @@
+from Sort.BubbleSort import *
+from typing import Any
+import copy
+
+
+
+class Main :
+    def main() -> None :
+        """ Main Method """
+        
+        print("Hello Wolrd!")
+        print()
+
+
+
+
+
+
+        pass
+
+
+
+
+
+
+
+
+if (__name__ == "__main__") :
+    Main.main()		
