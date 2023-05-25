82021133e126c55a0d5c91b5702742779f1c5b0b
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-05-26 00:30:20 +0900

    2023.05.26

--- Python Baekjoon/b9506/main_9506.py
+++ Python Baekjoon/b9506/main_9506.py
@@ -1,1 +1,25 @@
 ## 약수들의 합
+while(True) :
+    l = 0
+    x = list()
+    n = int(input())
+
+    if (n == -1) :
+        break
+
+    for i in range(1, n) :
+        if (n % i == 0) :
+            l += i
+            x.append(i)
+
+    if (l == n) :
+        print(f"{n} =", end="")
+
+        for j in x :
+            print(f" {j}", end="")
+
+            if (j != x[len(x)-1]) :
+                print(" +", end="")
+        print()
+    else :
+        print(f"{n} is NOT perfect.")
