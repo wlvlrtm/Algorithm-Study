52877d8fa84cc646caab66a97eeee2912c85a410
Seung-Ki Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-06-07 23:10:28 +0900

    2023.06.07

---
+++ Python Baekjoon/b24313/main_24313.py
@@ -0,0 +1,12 @@
+## 알고리즘 수업 - 점근적 표기 1
+a1, a0 = map(int, input().split())
+c = int(input())
+n0 = int(input())
+
+fn = a1 * n0 + a0
+gn = n0
+
+if (fn <= c * gn and a1 <= c) :
+    print(1)
+else :
+    print(0)
