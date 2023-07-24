c746fb4757b0a6f2e6aa7c7d8382fa75507d35c9
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-07-24 19:47:35 +0900

    2023.07.24

--- Python Baekjoon/b1003/main_1003.py
+++ Python Baekjoon/b1003/main_1003.py
@@ -1,19 +1,24 @@
 ## 피보나치 함수
 import sys
 
-def fibonacci(n : int) :
-    
+z = [1, 0, 1]
+o = [0, 1, 1]
 
+def fibonacci(n : int) :
+    l = len(z)
+
+    if (n >= l) :
+        for i in range(l, n + 1) :
+            z.append(z[i - 1] + z[i - 2])
+            o.append(o[i - 1] + o[i - 2])
+
+    print(z[n], o[n])
 
 def main() :
     t = int(sys.stdin.readline().strip())
-    l = list()
 
     for _ in range(t) :
-        l.append(int(sys.stdin.readline().strip()))
-
-    for i in l :
-        fibonacci(i)
+        fibonacci(int(sys.stdin.readline().strip()))
 
 
 
