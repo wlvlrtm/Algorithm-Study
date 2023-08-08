e8073eb3de48b9c24a9fd5a707a33f637e6cc552
Seung-Ki, Kim <62886544+wlvlrtm@users.noreply.github.com>
2023-08-08 23:34:38 +0900

    2023.08.08

---
+++ Python Baekjoon/b11727/main_11727.py
@@ -0,0 +1,19 @@
+## 2xn 타일링 2
+import sys
+
+def main() :
+    n = int(sys.stdin.readline().strip())
+    dp = [0] * 1001
+
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1) :
+        dp[i] = dp[i - 1] + 2 * dp[i - 2]
+
+    print(dp[n] % 10007)
+
+
+
+if (__name__ == "__main__") :
+    main()
